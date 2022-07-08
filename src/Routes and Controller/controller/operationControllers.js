3a64ca59dac23fc6649d3f1ef49242ab20c4ef68
Nicolas Clementi <clementinicolas@gmail.com>
2022-07-08 19:45:11 -0300

    cors con esperanza

--- src/Routes and Controller/controller/operationControllers.js
+++ src/Routes and Controller/controller/operationControllers.js
@@ -18,47 +18,73 @@ const operations ={
     .catch(error => console.log(error))
     
     },
-    edit: async (req,res)=>{
+    // edit: async (req,res)=>{
 
-        await db.Operation.findByPk(req.params.id)
-        .then(() =>
-                {
-                    db.Operation.update(
-                    {   
-                        description:req.body.description,
-                        date:req.body.date,
-                        amount: req.body.amount,
-                        id_type:req.body.type             
-                    },
-                    {
-                        where: {id : req.params.id}
-                    })
-        .then(()=>
-                res.send( "editado")
-        )
-        }).catch(error=>console.log(error))
-        },
+    //     await db.Operation.findByPk(req.params.id)
+    //     .then(() =>
+    //             {
+    //                 db.Operation.update(
+    //                 {   
+    //                     description:req.body.description,
+    //                     date:req.body.date,
+    //                     amount: req.body.amount,
+    //                     id_type:req.body.type             
+    //                 },
+    //                 {
+    //                     where: {id : req.params.id}
+    //                 })
+    //     .then(()=>
+    //             res.send( "editado")
+    //     )
+    //     }).catch(error=>console.log(error))
+    //     },
 
-        create: async (req,res)=>{
-            await db.Operation.create(
-                {
-                
-                    description:req.body.description,
+edit: async (req, res) => {
+  try {
+    await db.Operation.update({
+        description: req.body.description,
+        date: req.body.date,
+        amount: req.body.amount,
+        id_type: req.body.type
+    }, {
+        where: {
+            id: parseInt(req.params.id)
+        }
+    })
+    return res.status(200).json({
+        msg:"ok"
+    })
+  } catch (error) {
+    console.log(error);
+  }
+    
+},
+
+
+        create: async (req, res) => {
+            await db.Operation.create({
+
+                    description: req.body.description,
                     amount: req.body.amount,
-                    date:req.body.date,
-                    id_type:req.body.type,
-                    id_user:req.session.userLogged.id||114// hardcodeado hasta implementar  registro desde cliente ... realizar  registro desde http://localhost:3003/
-                    
-            
-                    
-                },{include:[{association:'types'},{association:'users'}]
-            })
-            .then(() =>{
-                
-                res.send("creado")
-            })
-                .catch(e=>console.log(e))
-            
+                    date: req.body.date,
+                    id_type: req.body.type,
+                    id_user: req.session.userLogged?.id || 114 // hardcodeado hasta implementar  registro desde cliente ... realizar  registro desde http://localhost:3003/
+
+
+
+                }, {
+                    include: [{
+                        association: 'types'
+                    }, {
+                        association: 'users'
+                    }]
+                })
+                .then(() => {
+
+                    res.send("creado")
+                })
+                .catch(e => console.log(e))
+
         },
         del:async(req,res)=>{
             db.Operation.destroy({   
